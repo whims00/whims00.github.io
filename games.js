984d08416ae6d9b7410d37fd0a731aff992484c4
Whims <whimsicalgd@gmail.com>
2026-03-03 10:04:26 +1100

    Version 1.0

---
+++ games.js
@@ -0,0 +1,44 @@
+const gamesList = [
+  {
+    title: "Granny",
+    img: "images/Granny.png",
+    link: "Granny.html",
+    desc: "Escape the haunted house without getting caught.",
+    external: false
+  },
+  {
+    title: "Drive Mad",
+    img: "images/DriveMad.png",
+    link: "DriveMad.html",
+    desc: "Conquer wild terrain in this physics-based driving challenge.",
+    external: false
+  },
+  {
+    title: "Snow Rider 3D",
+    img: "images/SnowRider.png",
+    link: "SnowRider.html",
+    desc: "Shred the slopes and dodge obstacles in this winter thrill ride.",
+    external: false
+  },
+  {
+    title: "Stickman Hook",
+    img: "images/StickmanHook.png",
+    link: "StickmanHook.html",
+    desc: "Swing through levels with perfect timing and momentum.",
+    external: false
+  },
+  {
+    title: "Retro Bowl",
+    img: "images/RetroBowl.png",
+    link: "RetroBowl.html",
+    desc: "Classic football meets arcade gameplay. Build your dynasty.",
+    external: false
+  },
+  {
+    title: "Tunnel Rush",
+    img: "images/TunnelRush.png",
+    link: "https://tunnelrush.online/",
+    desc: "Speed through neon tunnels and dodge incoming walls.",
+    external: true
+  }
+];
