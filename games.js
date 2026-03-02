0c378c78c6e2f3d51822fa97cb92b8463353c50e
Whims <whimsicalgd@gmail.com>
2026-03-03 10:39:34 +1100

    Version 1.1

--- games.js
+++ games.js
@@ -4,6 +4,7 @@ const gamesList = [
     img: "images/Granny.png",
     link: "Granny.html",
     desc: "Escape the haunted house without getting caught.",
+    tags: ["horror", "survival", "stealth"],
     external: false
   },
   {
@@ -11,6 +12,7 @@ const gamesList = [
     img: "images/DriveMad.png",
     link: "DriveMad.html",
     desc: "Conquer wild terrain in this physics-based driving challenge.",
+    tags: ["car", "driving", "physics"],
     external: false
   },
   {
@@ -18,6 +20,7 @@ const gamesList = [
     img: "images/SnowRider.png",
     link: "SnowRider.html",
     desc: "Shred the slopes and dodge obstacles in this winter thrill ride.",
+    tags: ["action", "3d", "sports"],
     external: false
   },
   {
@@ -25,6 +28,7 @@ const gamesList = [
     img: "images/StickmanHook.png",
     link: "StickmanHook.html",
     desc: "Swing through levels with perfect timing and momentum.",
+    tags: ["platformer", "action", "skill"],
     external: false
   },
   {
@@ -32,6 +36,7 @@ const gamesList = [
     img: "images/RetroBowl.png",
     link: "RetroBowl.html",
     desc: "Classic football meets arcade gameplay. Build your dynasty.",
+    tags: ["sports", "retro", "football"],
     external: false
   },
   {
@@ -39,6 +44,7 @@ const gamesList = [
     img: "images/TunnelRush.png",
     link: "https://tunnelrush.online/",
     desc: "Speed through neon tunnels and dodge incoming walls.",
+    tags: ["action", "skill", "arcade"],
     external: true
   }
 ];
